5a0dbacc69524a883946b1d4ef7d536a1c915c65
Melle <93864179+mellever@users.noreply.github.com>
2024-03-15 14:36:22 +0100

    Update difference_plotter.py

--- difference_plotter.py
+++ difference_plotter.py
@@ -1,17 +1,29 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-
-diff_arr = np.loadtxt("/home/melle/Documents/Deltares/rtc-tools-examples/cascading_channels/output/difference.txt")
+solution_hom = np.loadtxt("/home/melle/Documents/Deltares/rtc-tools-examples/cascading_channels/output/solution.txt")
+solution_ss = np.loadtxt("/home/melle/Documents/Deltares/rtc-tools-examples/cascading_channels/output/solution_ss.txt")
 key_list = np.loadtxt("/home/melle/Documents/Deltares/rtc-tools-examples/cascading_channels/output/keylist.txt", dtype='str')
-t = np.arange(1,len(diff_arr[0])+1)
 
+data_path = "rtc-tools-examples/cascading_channels/output/timeseries_export.csv"
+record = np.recfromcsv(data_path, encoding=None)
+
+c = "UpperChannel"
+solution = record[c.lower() + "h1"]
+
+solution_hom = solution_hom[1:]
+solution = solution[1:]
+t = np.arange(1,len(solution_hom)+1)
 
 plt.figure()
-for i in range(len(diff_arr)): plt.plot(t, diff_arr[i], label=str(key_list[i]))
-plt.xlabel("t")
-plt.ylabel("difference")
-plt.title("Difference plot")
+plt.plot(t,solution_hom, label="Homotopy")
+plt.plot(t,solution_ss, label="Smart seed")
+plt.scatter(t,solution, label="Actual solution", c='r')
+plt.xlabel("t [h]")
+plt.ylabel("h [m]")
+plt.title(key_list)
 plt.grid()
 plt.legend()
+plt.savefig("/home/melle/Pictures/UpperChannelDifferentInitial.pdf")
 plt.show()
+
