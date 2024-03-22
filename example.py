59faec0c9a5296f141c9b167c422d79810595eb2
Melle <93864179+mellever@users.noreply.github.com>
2024-03-22 11:09:04 +0100

    Update example.py

--- example.py
+++ example.py
@@ -45,7 +45,6 @@ class Example(
 
     def pre(self):
         
-        
         super().pre()
         # print(self.get_timeseries("Inflow_Q"))
         # exit(1)
@@ -84,7 +83,7 @@ class Example(
             
     def homotopy_options(self):
         options = super().homotopy_options()
-        number_of_linear_timesteps = 12
+        number_of_linear_timesteps = 0
         options['non_linear_thresh_time_idx'] = len(self.io.datetimes)-number_of_linear_timesteps
         options['delta_theta_0'] = 0.5
         options['delta_theta_min'] = 0.010
@@ -137,10 +136,53 @@ class Example(
         # print(self.get_timeseries("Inflow_Q"))
         # exit(1)
         
-
-
 # Run
 import timeit
-starttime = timeit.default_timer()
-run_optimization_problem(Example)
-print("RUNTIME :", timeit.default_timer() - starttime,"s")
+
+initial_data = "/home/melle/Documents/Deltares/rtc-tools-examples/cascading_channels/input/initial_state.csv"
+
+upperCh1 = np.linspace(0.85, 1.35, 3)
+upperCh2 = np.linspace(0.85, 1.35, 3)
+middleCh1 = np.linspace(0.35, 0.85, 3)
+middleCh2 = np.linspace(0.35, 0.85, 3)
+lowerCh1 = np.linspace(-0.15, 0.35, 3)
+lowerCh2 = np.linspace(-0.15, 0.35, 3)
+
+
+c1, c2, c3, c4, c5, c6 = np.meshgrid(upperCh1, upperCh2, middleCh1, middleCh2, lowerCh1, lowerCh2)
+c1 = c1.flatten()
+c2 = c2.flatten()
+c3 = c3.flatten()
+c4 = c4.flatten()
+c5 = c5.flatten()
+c6 = c6.flatten()
+
+
+initial_list = []
+for i in range(len(c1)):
+    initial = str(c1[i])+","+str(c2[i])+","+str(c3[i])+","+str(c4[i])+","+str(c5[i])+","+str(c6[i])
+    initial_list.append(initial)
+
+time_array = np.empty_like(initial_list, dtype=float)
+
+for i in range(len(initial_list)):
+
+    #Change initial conditions
+    with open(initial_data,'r',encoding='utf-8') as file:
+        data = file.readlines()
+
+    data[1] = initial_list[i]
+
+    with open(initial_data, 'w', encoding='utf-8') as file: 
+        file.writelines(data) 
+
+    try:
+        starttime = timeit.default_timer()
+        run_optimization_problem(Example)
+        runtime = timeit.default_timer() - starttime
+        time_array[i] = runtime
+    except: 
+        time_array[i] = np.nan
+    if i==2: break
+
+np.savetxt("/home/melle/Documents/Deltares/rtc-tools-examples/cascading_channels/output/times_ss.txt", time_array)
